fe60e077fc0f4536d4af940edf70141d1e117e04
lanou3g <lanou3g@xiongmao.local>
2014-03-03 19:42:37 +0800

    Initial Commit

---
+++ News/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by lanou3g on 14-3-3.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
